a800518d8f53c0d79f7bcc7d65beab894cc53dd0
Jon Sadowski <jwsadowski@gmail.com>
2022-09-19 22:01:11 -0500

    checkword

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,42 @@ var wordBlanksEl = document.querySelector(".word-blanks")
 var words = ["javascript", "variable", "object", "python", "local storage", "timeout", "interval"]
 var word = 
 
+var validChars = 'abcdefghijklmnopqrstuvwxyz'
+
+var guessedCharacters = []
+
+var score = 0
+
+var wordFromDom 
+
+function checkWord() {
+    var wordFromDom = wordBlanksEl.textContent.split(' ').join(' ')
+}
+
+function handleKeydown(event) {
+    console.log(event.key)
+    if (validChars.includes(event.key)) {
+        guessedCharacters.push(event.key) 
+        renderChars()
+    }
+}
+
 
 function renderChars() {
     console.log(word);
     var str = ""
     for (var i = 0; i < word.length; i++) {
-        str += "_"
-        console.log(str);
+    var letter = word[i]   
+        if (guessedCharacters.includes(letter))
+        str += letter + " "
+        else {
+            str += " "
+        }
     }
 } 
 
 wordBlanksEl.textContent = str.trim();
+checkWord()
 
 function startRound() {
     var RandomWord = Math.floor( (Math.random) * words.length) 
@@ -24,4 +49,7 @@ function startRound() {
 }
 
 startBtn.addEventListener('click', startRound); 
+document.addEventListener('keydown', handleKeydown); {
+    console.log()
+}
 
